1fd1c441f728e1d2059d9eccf2854ea8d600e476
Jacob Johnson <jacob@Jacobs-MacBook-Pro.local>
2023-03-19 06:19:08 -0400

    modified pytesseract call

--- inception_resnetv2_predictions.py
+++ inception_resnetv2_predictions.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ad55fa337da6d86254449f85c2ee3c5adfbb713f480645649d0eba688816e3d3
-size 1853
+oid sha256:93021d2e65d5406bc04d1308d79527f4972f3e3e4775dae3159bdf1a84e9661e
+size 1808
